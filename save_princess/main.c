347e6e40b9f522f621a463e34e715581adfbe95e
parkhwakyoung <firenoah.m@gmail.com>
2022-12-30 23:38:53 +0900

    text색깔 설정

--- save_princess/main.c
+++ save_princess/main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include<Windows.h> 	//À©µµ¿ì¿¡¼­ »ö±ò ÀüÈ¯
+#include <stdlib.h>
+#include <time.h>
+#define YELLOW2 14
+#define BLACK 0
+
+
+// ±Û¾¾ »ö±ò, ¹è°æ»ö±ò
+void textcolor(int t_color, int bg_color) {
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), t_color | bg_color << 4);
+};
 
 int main() {
+	textcolor(YELLOW2, BLACK);
 
 	return 0;
 }
