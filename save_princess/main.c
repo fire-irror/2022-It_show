2f1cbfbda2b70df4d5be3e1d4be62ecb21869b7b
parkhwakyoung <firenoah.m@gmail.com>
2023-01-23 22:35:20 +0900

    playermove  나오게 하기. (발자취 없애는 건 아직..)

--- save_princess/main.c
+++ save_princess/main.c
@@ -11,7 +11,7 @@ struct Player {
 	int speed_max;
 };
 
-void playerDraw() {
+void PlayerMove() {
 	printf("■");
 }
 
@@ -22,7 +22,19 @@ int main() {
 	player.speed = 7;//플레이어 속도
 	player.score = 0;//플레이어 점수
 	player.life = 10;
+
+	while (1) {
+		if (GetAsyncKeyState(VK_LEFT))
+			PlayerMove();
+		else if (GetAsyncKeyState(VK_RIGHT))
+			PlayerMove();
+		else if (GetAsyncKeyState(VK_UP))
+			PlayerMove();
+		else if (GetAsyncKeyState(VK_DOWN))
+			PlayerMove();
+		else if (GetAsyncKeyState(VK_SPACE))//엔터를 눌렀을 때
+			PlayerMove();
+	}
 	
-	system("cls");
-	playerDraw();
+	return 0;
 }
