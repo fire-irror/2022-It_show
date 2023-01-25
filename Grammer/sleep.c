4c14e31a3bc002796988c2367478527f786a3450
parkhwakyoung <firenoah.m@gmail.com>
2023-01-25 21:19:41 +0900

    [문법] sleep 함수

---
+++ Grammer/sleep.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <windows.h>
+
+int  main(void) {
+	for (int i = 0; i < 3; i++) {
+		printf("Hello");
+		Sleep(1000);
+	}
+	return 0;
+}
--- save_princess/main.c
+++ save_princess/main.c
@@ -11,7 +11,13 @@ struct Player {
 	int speed_max;
 };
 
+
+void gotoxy(int x, int y) {
+
+}
+
 void PlayerMove() {
+	gotoxy(14, 28);
 	printf("■");
 }
 
@@ -25,16 +31,17 @@ int main() {
 
 	while (1) {
 		if (GetAsyncKeyState(VK_LEFT))
-			PlayerMove();
+			PlayerMove(-1, 0);
 		else if (GetAsyncKeyState(VK_RIGHT))
 			PlayerMove();
 		else if (GetAsyncKeyState(VK_UP))
-			PlayerMove();
+			PlayerMove(0,-1);
 		else if (GetAsyncKeyState(VK_DOWN))
-			PlayerMove();
+			PlayerMove(0,player.speed);
 		else if (GetAsyncKeyState(VK_SPACE))//엔터를 눌렀을 때
-			PlayerMove();
+			PlayerMove(player.speed,0);
 	}
+	system.clear(Color::BLACK);
 	
 	return 0;
 }
