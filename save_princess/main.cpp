a838de924b47b94f7a4feb80bd97c242a9ae5766
parkhwakyoung <firenoah.m@gmail.com>
2023-01-27 23:54:41 +0900

    총알 구현중....

--- save_princess/main.cpp
+++ save_princess/main.cpp
@@ -1,18 +1,39 @@
 ﻿#include <stdio.h>
 #include <Windows.h>
+#include <stdlib.h>
+#include <time.h>
 #include "console.h"
 
 struct Player {
 	int speed;
 	float x, y;
+	int life;
 };
+
+struct Bullet {
+	int x, y;
+	int is_fired;	//발사 여부
+};
+
+const int BULLET_NUM = 50;
 
 int main() {
 
-	struct Player player;
-	player.speed = 600;		//TODO: player_speed해결하기.
-	player.x = 60, player.y = 15;
 
+	struct Player player;
+	player.speed = 10;		//TODO: player_speed해결하기.
+	player.x = 60;
+	player.y = 15;
+	player.life = 3;
+
+	struct Bullet bullet;
+	int is_fired;
+	bullet.x = 0;
+	bullet.y = 0;
+
+	long start_time = clock();	// 게임 시작 시간
+	long spent_time;			// 게임 진행 시간
+	int fired_time = 0;
 
 	while (1) {
 
@@ -38,16 +59,25 @@ int main() {
 			}
 		}
 		else if (GetAsyncKeyState(VK_DOWN)) {
-			 player.y++;
-			 if (player.y > 29) {
-				 player.y = 29;
-			 }
+			player.y++;
+			if (player.y > 29) {
+				player.y = 29;
+			}
 		}
+		else if (GetAsyncKeyState(VK_SPACE)) {
+			bullet.x = player.x;
+			bullet.y = player.y - 1;
+
+		}
+
+		gotoxy(bullet.x, bullet.y);
+		printf("▲");
+
 		gotoxy(player.x, player.y);
 		printf("■");
 		Sleep(50);
 	}
 
 	system("pause");
-	return 0;
+	return 0
 }
